7f70eb11977d32c77c916a3eef23880c23a8db62
Alin Tomescu <alinush@mit.edu>
2017-04-21 11:05:07 -0400

    added dependencies for OS X

---
+++ osx-deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cd src/
+meteor npm install --save babel-runtime angular-animate angular-meteor angular-sanitize angular-ui-router ionic-scripts angular angular-ecmascript moment
