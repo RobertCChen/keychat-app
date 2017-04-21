d7a4d066e5a8fd288b6df93b4f92c8c42b0ec5b5
Alin Tomescu <alinush@mit.edu>
2017-04-21 17:07:09 -0400

    finished step 4 but something wrong with css

--- src/client/scripts/routes.js
+++ src/client/scripts/routes.js
@@ -1,22 +1,32 @@
 import { Config } from 'angular-ecmascript/module-helpers';
  
 import chatsTemplateUrl from '../templates/chats.html';
+import chatTemplateUrl from '../templates/chat.html';
 import tabsTemplateUrl from '../templates/tabs.html';
  
 export default class RoutesConfig extends Config {
   configure() {
     this.$stateProvider
-      .state('tab', {
+      .state('tab', { // TODO: What is this 'tab' name for?
         url: '/tab',
-        abstract: true,
+        abstract: true, // TODO: Abstract??
         templateUrl: tabsTemplateUrl
       })
-      .state('tab.chats', {
+      .state('tab.chats', { // TODO: naming again
         url: '/chats',
         views: {
-          'tab-chats': {
+          'tab-chats': { // TODO: we are naming this again?
             templateUrl: chatsTemplateUrl,
-            controller: 'ChatsCtrl as chats',
+            controller: 'ChatsCtrl as chats', // TODO: 'chatsctrl' and 'chats'
+          }
+        }
+      })
+      .state('tab.chat', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: chatTemplateUrl,
+            controller: 'ChatCtrl as chat'
           }
         }
       });
