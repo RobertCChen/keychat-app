12f93a98db303af4b83993d765cd7f6f8d4d52b0
Alin Tomescu <alinush@mit.edu>
2017-05-07 17:46:18 -0400

    pushed HTTP request changes, but they don't work because Keybase doesn't support CORS

--- src/client/scripts/controllers/login.controller.js
+++ src/client/scripts/controllers/login.controller.js
@@ -4,23 +4,56 @@ import { Controller } from 'angular-ecmascript/module-helpers';
 import scrypt from 'scryptsy';
  
 export default class LoginCtrl extends Controller {
-  login() {
-    if (_.isEmpty(this.username)) return;
-    if (_.isEmpty(this.password)) return;
-
-    // Yaaaay! This seems to work!
-    // TODO: Tune the scrypt parameters to be the same as Keybase's
-    // (see https://www.npmjs.com/package/js-scrypt)
+  sendLoginHttpRequest(username, password) {
+    // The scrypt parameters here are the same as Keybase's
     var N = 32768;
     var r = 8;
     var p = 1;
     var len = 256;
-    var resultBuffer = scrypt('paswd', 'salt', N, r, p, len);
-    this.$log.info('scrypt: ' + resultBuffer);
- 
-    // TODO: derive pdpka4 and 5 from scrypt(password)
-    // TODO: send HTTPS POST request w/ username and pdkpa's
-    // TODO: get the session cookie and save it
+
+    // TODO: Keybase and CORS. We'll be running on mobile, so why can't we test this from localhost in the browser?
+    // https://github.com/keybase/keybase-issues/issues/620
+    // https://github.com/keybase/keybase-issues/issues/1959
+    // https://github.com/keybase/keybase-issues/issues/625
+
+    // Q: What is the pdpka_login parameter? It doesn't seem to be documented in getsalt docs
+    // A: I see it used here: https://github.com/keybase/libweb/blob/master/src/hilevel/account.iced
+    var apiUrl = 'https://keybase.io/_/api/1.0/getsalt.json?email_or_username=' + username + '&pdpka_login=true';
+
+    // query the API
+    HTTP.get(apiUrl, function(err, result) {
+      if(err) {
+        console.log("error in HTTP.get callback: " + err);
+        return
+      }
+
+      console.log("result is " + result.content);
+
+      var salt = JSON.parse(result.content).salt;
+      console.log("salt is " + salt);
+
+      var login_session = JSON.parse(result.content).login_session;
+      console.log("login_session is " + login_session);
+
+      var bytes = [];
+      for(var i=0; i< salt.length-1; i+=2){
+          bytes.push(parseInt(salt.substr(i, 2), 16));
+      }
+      var binarySalt = String.fromCharCode.apply(String, bytes);
+      var passwordStream = scrypt(password, binarySalt, N, r, p, len);
+      this.$log.info('scrypt: ' + resultBuffer);
+
+      // TODO: derive pdpka4 and 5 from scrypt(password)
+      // TODO: send HTTPS POST request w/ username and pdkpa's
+      // TODO: get the session cookie and save it
+    });
+  }
+
+  login() {
+    if (_.isEmpty(this.username)) return;
+    if (_.isEmpty(this.password)) return;
+
+    this.sendLoginHttpRequest(this.username, this.password);
 
     Meteor.loginWithPassword(this.username, this.password, (err) => {
       if (err) return this.handleError(err);
